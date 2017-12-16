9c422a6e80daf0937ae50ffa8cd912916db2bfa9
sebseb7 <github@seb.exse.net>
2017-12-16 05:34:55 +0100

    pl

---
+++ msr/config.js
@@ -0,0 +1,17 @@
+var api = "http://78.47.92.206:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "sebseb7.github.io";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support";
+
+var cryptonatorWidget = ["XMR-BTC"];
+
+var easyminerDownload = "http/releases/";
+
+var blockchainExplorer = "/block/";
+
+var transactionExplorer = "/transaction/";
