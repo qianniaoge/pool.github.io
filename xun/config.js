0f557506a05b24462420733e746cdecb7c273cd4
sebseb7 <github@seb.exse.net>
2017-12-18 03:06:35 +0100

    xun pool

---
+++ xun/config.js
@@ -0,0 +1,15 @@
+var api = "http://78.47.92.206:8118";
+
+var poolHost = "78.47.92.206";
+
+var cryptonatorWidget = ["BTC-USD","{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "http://explorer.ultranote.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.ultranote.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
