cd19addeca572091a86fd8923db8865afb8cc7ba
sebseb7 <github@seb.exse.net>
2017-12-18 21:38:49 +0100

    wallet

--- msr/config.js
+++ msr/config.js
@@ -8,9 +8,10 @@ var irc = "";
 
 var email = "";
 
-
 var blockchainExplorer = "http://www.msrchain.net/block/{id}";
 
 var transactionExplorer = "http://www.msrchain.net/tx/{id}";
+
+var guiWallet = "https://getmasari.org/#gui-wallet";
 
 var themeCss = "themes/nightly-mining-dark-theme.css";
