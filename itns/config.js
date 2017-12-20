f99964b419738d39c6acf6281efcb519f5c75670
sebseb7 <github@seb.exse.net>
2017-12-20 01:41:39 +0100

    itns

---
+++ itns/config.js
@@ -0,0 +1,19 @@
+var api = "http://78.46.85.142:8889";
+
+var poolHost = "78.46.85.142";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "";
+
+var transactionExplorer = "";
+
+var guiWallet = "";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
