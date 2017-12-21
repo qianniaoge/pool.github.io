fccc80b9bb5386e5b29aa483b654491e478f0c35
sebseb7 <github@seb.exse.net>
2017-12-21 02:47:19 +0100

    karbo

---
+++ karbo/config.js
@@ -0,0 +1,21 @@
+var api = "http://78.46.85.142:29999";
+
+var poolHost = "78.46.85.142";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "";
+
+var transactionExplorer = "";
+
+var guiWallet = "";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+
