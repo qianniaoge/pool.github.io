88d290a79e54eeb997e2a2dc7e3ecdcb55c59052
sebseb7 <github@seb.exse.net>
2017-12-30 12:11:07 +0100

    fix

--- b2b/config.js
+++ b2b/config.js
@@ -11,7 +11,7 @@ var email = "";
 var blockchainExplorer = "";
 var transactionExplorer = "";
 
-var guiWallet = "";
+var guiWallet = "https://b2bcoin.xyz/assets/wallets/b2bcoin-wallet-windows-64.zip";
 
 var paperWallet = "";
 
